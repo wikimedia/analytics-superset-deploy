990bc3815a1d97e7b57147c43b5fe67df64400ad
Ottomata <aotto@wikimedia.org>
2018-01-04 14:36:25 -0500

    Update build_wheels.sh to python3; update build artifacts for python3
    create_virtualenv.sh shouldn't use --system-site-packages, as
    installed numpy can conflict with superset's panda's numpy.
    
    Bug: T182688
    
    Change-Id: Ifbcd0e8aa2959f78a9c1c2daa0cda623a3c18663

--- build_wheels.sh
+++ build_wheels.sh
@@ -3,28 +3,50 @@
 # Install all dependencies into artifacts/$dist.  This should only be run
 # on a build server.
 
+set -ex
+
 # source common variables
 source $(dirname $0)/profile.sh
 
+# Choose name for libmysqlclient-dev based on distribution name.
+if [ "${dist}" == "jessie" ]; then
+    libmysqlclient_dev_package='libmysqlclient-dev'
+else
+    libmysqlclient_dev_package='default-libmysqlclient-dev'
+fi
+
 sudo apt-get --yes install \
-  python-pip \
-  python-wheel \
-  python-dev \
+  python3-pip \
+  python3-wheel \
+  python3-dev \
+  virtualenv \
   build-essential \
   libssl-dev \
   libffi-dev \
   libsasl2-dev \
   libldap2-dev \
-  libmysqlclient-dev
-
+  $libmysqlclient_dev_package
 
 # If you update any requirements, you should also update this timestamp.
 # This makes it more likely that wheels built with the same versions
 # will be byte for byte identical.
-export SOURCE_DATE_EPOCH=1512574740
-
+export SOURCE_DATE_EPOCH=1515093288
 
 rm -rf $wheels_dir
 mkdir -p $wheels_dir
 
-pip wheel -w $wheels_dir -r $(dirname $0)/frozen-requirements.txt
+
+
+# Build in a virtualenv, but install wheel files to $wheels_dir.
+build_venv=/tmp/superset-build-venv
+test -e $build_venv && rm -rf $build_venv
+virtualenv --python python3 --system-site-packages $build_venv
+
+# Weird bug where numpy needs to be installed before pandas is built.
+# https://github.com/pandas-dev/pandas/issues/16715#issuecomment-309498415
+# Build this wheel first and install it into our build virtualenv.
+# This will let pandas build against the version of numpy that we will
+# actually install.
+$build_venv/bin/pip wheel -w $wheels_dir numpy
+$build_venv/bin/pip install --no-index --find-links $wheels_dir numpy
+$build_venv/bin/pip wheel -w $wheels_dir -r $(dirname $0)/frozen-requirements.txt
--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -6,10 +6,9 @@ set -ex
 source $(dirname $0)/profile.sh
 
 # remove any existing virtualenv
-rm -rfv $venv
+test -e $venv && echo "Deleting existent virtualenv at $venv" && rm -rf $venv
 mkdir -pv $venv
 
-echo "Creating new virtualenv at $venv with wheels from $wheels_dir..."
-# create a new virtualenv from wheels/
-virtualenv --system-site-packages --never-download $venv
+echo "Creating new virtualenv at $venv with requirements from wheels in $wheels_dir..."
+virtualenv --python python3 --never-download $venv
 $venv/bin/pip install --no-index --find-links $wheels_dir --requirement $deploy_dir/frozen-requirements.txt
