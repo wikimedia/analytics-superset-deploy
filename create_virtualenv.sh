afbba9b2b79050f3df9e86ffbbcedd4d5c11a0a9
Ottomata <aotto@wikimedia.org>
2019-01-16 10:40:00 -0500

    Fix create_virtualenv.sh to build from $wheels_dir directly
    instead of from frozen-requirements.txt.
    This can be reverted if we switch back to a regular
    apache release, rather than our fork on github.
    
    Change-Id: I102ffcb6746b24ead54eff8d824ea23a30dc1b38

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -9,6 +9,11 @@ source $(dirname $0)/profile.sh
 test -e $venv && echo "Deleting existent virtualenv at $venv" && rm -rf $venv
 mkdir -pv $venv
 
-echo "Creating new virtualenv at $venv with requirements from wheels in $wheels_dir..."
+echo "Creating new virtualenv at $venv with all wheels in $wheels_dir..."
 virtualenv --python python3 --never-download $venv
-$venv/bin/pip install --no-index --find-links $wheels_dir --requirement $deploy_dir/frozen-requirements.txt
+# Until Wikimedia started using a fork, we used -r frozen-requirements.txt
+# to install superset and other requirements.  However, now that
+# frozen-requirements.txt is pointing at the wikimedia github fork url,
+# pip install with it will not be able to find superset in the wheels dir.
+# Instead, just install all wheels in the wheels dir.
+$venv/bin/pip install --no-index --find-links $wheels_dir $wheels_dir/*.whl
